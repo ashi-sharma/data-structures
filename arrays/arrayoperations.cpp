ea477929a670e96b32145551fd96fb1b0e90770b
Ashi Sharma <57591881+ashi-sharma@users.noreply.github.com>
2020-12-22 00:00:07 +0530

    Update arrayoperations.cpp

--- arrays/arrayoperations.cpp
+++ arrays/arrayoperations.cpp
@@ -30,7 +30,66 @@ class array{
 		void reverse();
 		T min();
 		T max();
+		void append(T ele);
+		int linsrch(T key);
+		int binarysearch(T ele);
+		T get(int index);
+		void set(int index, T e);
+		T sum();
 };
+
+template <class T>
+T array<T>::sum(){
+	T s=0;
+	for(int i=0;i<length;i++){
+		s=s+a[i];
+	}
+	return s;
+}
+
+template <class T>
+T array<T>::get(int index){
+	if(index<length){
+		return a[index];
+	}
+}
+
+template <class T>
+void array<T>::set(int index, T e){
+	if(index<=length){
+		a[index] = e;
+	}
+	display();
+}
+
+template <class T>
+int array<T>::binarysearch(T ele){
+	int first = 0,last = length-1,mid = last/2;
+	while(mid<last && mid>first){
+		if(a[mid] == ele)
+			return mid;
+		else if(a[mid]<ele){
+			first = mid+1;
+			mid = (last + first)/2;
+		}
+		else{
+			last = mid-1;
+			mid = (last+first)/2;
+		}
+	}
+	return -1;
+}
+
+template<class T>
+int array<T>::linsrch(T key){
+	if(length>0){
+		for(int i=0;i<length;i++){
+			if(key == a[i])
+			return i;
+		}
+	}
+	return -1;
+}
 
 template<class T>
 T array<T>::min(){
@@ -96,17 +155,69 @@ void array<T>::reverse(){
 	display();
 }
 
+template<class T>
+void array<T>::append(T ele){
+	if(length<size){
+		a[length] = ele;
+		length++;
+	}
+	display();
+}
+
 int main() {
 	array<int> arr;
-	arr.insert(0,1);
-	arr.insert(1,6);
-	arr.insert(2,4);
-	
-	arr.display();
-	arr.reverse();
-	cout<<arr.del(1)<<endl;
-	arr.display();
-	cout<<"minimum element="<<arr.min()<<endl;
-	cout<<"maximum element="<<arr.max()<<endl;
+	int choice,ind;
+	int num;          
+	do{
+		cout<<"******MAIN MENU*******"<<endl;
+		cout<<"1. Insert"<<endl;
+		cout<<"2. Delete"<<endl;
+		cout<<"3. Append"<<endl;
+		cout<<"4. Linear search"<<endl;
+		cout<<"5. Binary search"<<endl;
+		cout<<"6. Display"<<endl;
+		cout<<"7. Reverse"<<endl;
+		cout<<"8. Minimum element"<<endl;
+		cout<<"9. Maximum element"<<endl;
+		cout<<"10. Sum"<<endl;
+		cout<<"11. Get"<<endl;
+		cout<<"12. Set"<<endl;
+		cout<<"13. Exit"<<endl;
+		cout<<"Enter Your choice:"<<endl;
+		cin>>choice;
+		switch(choice){
+			case 1: cin>>ind>>num;
+					arr.insert(ind,num);
+					break;
+			case 2: cin>>ind;
+					cout<<arr.del(ind)<<"  deleted!"<<endl;
+					break;
+			case 3: cin>>num;
+					arr.append(num);
+					break;
+			case 4: cin>>num;
+					cout<<"found at "<<arr.linsrch(num)<<endl;
+					break;
+			case 5: cin>>num;
+					cout<<"element found at "<<arr.binarysearch(num)<<endl;
+					break;
+			case 6: arr.display();
+					break;
+			case 7: arr.reverse();
+					break;
+			case 8: cout<<arr.min()<<endl;
+					break;
+			case 9: cout<<arr.max()<<endl;
+					break;
+			case 10: cout<<arr.sum()<<endl;
+					 break;
+			case 11: cin>>ind;
+					 cout<<arr.get(ind)<<endl;
+					 break;
+			case 12: cin>>ind>>num;
+					 arr.set(ind,num);
+					 break;
+		}
+	}while(choice<13);
 	return 0;
 }
