972ae892b36b0308f376af592c5c0e8fc074002c
Ashi Sharma <57591881+ashi-sharma@users.noreply.github.com>
2021-01-04 00:45:28 +0530

    Update miscellaneous.cpp

--- arrays/miscellaneous.cpp
+++ arrays/miscellaneous.cpp
@@ -28,19 +28,27 @@ int main() {
 
 //************Minimum number of jumps to reach end of the array***************
 
-int minJumps(int arr[], int n){
-    int minjump[n]={10000001};
-    cout<<minjump[1];
-    minjump[0]=0;
-    cout<<endl<<INT_MAX<<" "<<minjump[2];
-    for(int i=1;i<n;i++){
-        for(int j=0;j<i;j++){
-            if(i<=j+arr[j]){
-                minjump[i]=std::min(minjump[i],minjump[j]+1);
+int minJumps(int arr[], int n)
+{
+    // jumps[n-1] will hold the result
+    int* jumps = new int[n];
+    int i, j;
+ 
+    if (n == 0 || arr[0] == 0)
+        return INT_MAX;
+ 
+    jumps[0] = 0;
+ 
+    // Find the minimum number of jumps to reach arr[i]
+    // from arr[0], and assign this value to jumps[i]
+    for (i = 1; i < n; i++) {
+        jumps[i] = INT_MAX;
+        for (j = 0; j < i; j++) {
+            if (i <= j + arr[j] && jumps[j] != INT_MAX) {
+                jumps[i] = min(jumps[i], jumps[j] + 1);
+                break;
             }
         }
     }
-    //if(minjump[n-1]==INT_MAX)
-    //    return -1;
-    return minjump[n-1];
+    return jumps[n - 1];
 }
