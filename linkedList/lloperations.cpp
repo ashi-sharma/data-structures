7a10d4b8cc7776c6160a328a02467b55a834965e
Ashi Sharma <57591881+ashi-sharma@users.noreply.github.com>
2021-01-20 23:46:23 +0530

    Update lloperations.cpp

--- linkedList/lloperations.cpp
+++ linkedList/lloperations.cpp
@@ -20,28 +20,68 @@ class linkedlist{
 		void Delete(int index);
 		void length();
 };
-
+linkedlist::linkedlist(int arr[],int n){
+	first=new node;
+	node *last,*t;
+	t=first;
+	last=first;
+	for(int i=0;i<n;i++){
+		t->data=arr[i];
+		last=new node;
+		t->next=last;
+		t=last;
+	}
+}
+linkedlist::~linkedlist(){
+	node *p;
+	p=first;
+	while(first){
+		first=first->next;
+		delete p;
+		p=first;
+	}
+}
 void linkedlist::insert(int index,int num){
-	new node n;
-	n.data=num;
-	n.next=NULL;
-	if(first==NULL){
-		first=n;
+	node *n = new node;
+	int i=0;
+	node *t, *p;
+	n->data=num;
+	n->next=NULL;
+	p=first;
+	while(p){
+		if(i==index){
+			n->next=t->next;
+			t->next=n;
+			break;
+		}
+		i++;
+		t=p;
+		p=p->next;
 	}
 }
 
 void linkedlist::display(){
-	
+	node *p;
+	p=first;
+	while(p->next!=NULL){
+		cout<<p->data<<" ";
+	}
 }
 		void Delete(int index);
 		void length();
 
 int main(){
-	linkedlist ll; 
-	ll.insert(0,6);
-	ll.insert(1,8);
-	ll.insert(2,2);
-	ll.insert(3,5);
-	ll.insert(4,1);
+	int a[1]={7};
+	linkedlist ll(a,1); 
+	cout<<"inserting 6"<<endl;
+	ll.insert(1,6);
+	ll.display();
+	cout<<"inserting 8"<<endl;
+	ll.insert(2,8);
+	ll.display();
+	ll.insert(3,2);
+	ll.insert(4,5);
+	ll.insert(5,1);
+	ll.display();
 	return 0;
 }
