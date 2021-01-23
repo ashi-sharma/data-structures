0f6a53d19c60d2199cad68c546308056b6af80d6
Ashi Sharma <57591881+ashi-sharma@users.noreply.github.com>
2021-01-23 14:22:19 +0530

    Update lloperations.cpp

--- linkedList/lloperations.cpp
+++ linkedList/lloperations.cpp
@@ -15,19 +15,22 @@ class linkedlist{
 		}
 		linkedlist(int arr[],int n);
 		~linkedlist();
-		void insert(int index,int num);
+		int insert(int index,int num);
 		void display();
-		void Delete(int index);
-		void length();
+		int Delete(int index);
+		int length();
 };
 linkedlist::linkedlist(int arr[],int n){
 	first=new node;
 	node *last,*t;
 	t=first;
 	last=first;
-	for(int i=0;i<n;i++){
-		t->data=arr[i];
+	first->data=arr[0];
+	first->next=NULL;
+	for(int i=1;i<n;i++){
 		last=new node;
+		last->data=arr[i];
+		last->next=NULL;
 		t->next=last;
 		t=last;
 	}
@@ -41,18 +44,23 @@ linkedlist::~linkedlist(){
 		p=first;
 	}
 }
-void linkedlist::insert(int index,int num){
+int linkedlist::insert(int index,int num){
 	node *n = new node;
 	int i=0;
 	node *t, *p;
 	n->data=num;
 	n->next=NULL;
+	if(index==0){
+		n->next=first;
+		first=n;
+		return num;
+	}
 	p=first;
 	while(p){
 		if(i==index){
 			n->next=t->next;
 			t->next=n;
-			break;
+			return num;
 		}
 		i++;
 		t=p;
@@ -63,25 +71,57 @@ void linkedlist::insert(int index,int num){
 void linkedlist::display(){
 	node *p;
 	p=first;
-	while(p->next!=NULL){
+	while(p){
 		cout<<p->data<<" ";
+		p=p->next;
+	}
+	cout<<endl;
+}
+int linkedlist::Delete(int index){
+	node *p,*t;
+	int num;
+	p=first;
+	if(index==0){
+		num=first->data;
+		first=first->next;
+		delete p;
+		return num;
+	}
+	t=first;
+	p=first->next;
+	for(int i=1;i<length();i++){
+		if(i==index){
+			num=p->data;
+			t->next=p->next;
+			delete p;
+			return num;
+		}
+		t=p;
+		p=p->next;
 	}
 }
-		void Delete(int index);
-		void length();
+int linkedlist::length(){
+	node *p=first;
+	int len=0;
+	while(p){
+		len++;
+		p=p->next;
+	}
+	return len;
+}
 
 int main(){
-	int a[1]={7};
-	linkedlist ll(a,1); 
+	int a[3]={7,10,13};
+	linkedlist ll(a,3); 
 	cout<<"inserting 6"<<endl;
 	ll.insert(1,6);
 	ll.display();
 	cout<<"inserting 8"<<endl;
 	ll.insert(2,8);
 	ll.display();
-	ll.insert(3,2);
-	ll.insert(4,5);
-	ll.insert(5,1);
+	cout<<endl<<"length = "<<ll.length()<<endl;
+	ll.Delete(2);
 	ll.display();
+	cout<<endl<<"length = "<<ll.length()<<endl;
 	return 0;
 }
