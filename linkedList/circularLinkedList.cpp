456aa3b8fc7e7f872de58b56049771fb78dfec15
Ashi Sharma <57591881+ashi-sharma@users.noreply.github.com>
2021-01-27 21:01:51 +0530

    Add files via upload

---
+++ linkedList/circularLinkedList.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class node{
+	public:
+		int data;
+		node *next;
+};
+class cll{
+	public:
+		node *head;
+		cll(){
+			head=NULL;
+		}
+		~cll()
+};
+int main(){
+	
+}
