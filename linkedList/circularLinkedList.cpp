d1495f16545eb27c345c57b88160271e71eec32f
Ashi Sharma <57591881+ashi-sharma@users.noreply.github.com>
2021-01-28 19:45:37 +0530

    Update circularLinkedList.cpp

--- linkedList/circularLinkedList.cpp
+++ linkedList/circularLinkedList.cpp
@@ -1,18 +1,57 @@
 #include<iostream>
 using namespace std;
+//class node
 class node{
 	public:
 		int data;
 		node *next;
 };
-class cll{
+//class circular linked list
+class circularlinkedlist{
 	public:
 		node *head;
-		cll(){
+		circularlinkedlist(){
 			head=NULL;
 		}
-		~cll()
+		~circularlinkedlist();
+		circularlinkedlist(int arr[],int n);
+		void display();
 };
-int main(){
-	
+//destructor
+circularlinkedlist::~circularlinkedlist(){
+	node *p=head;
+	while(head){
+		head=p->next;
+		delete p;
+		p=head;
+	}
 }
+//parameterised constructor definition
+circularlinkedlist::circularlinkedlist(int arr[],int n){
+	head=new node;
+	head->data=arr[0];
+	head->next=head;
+	node *p=head,*t;
+	for(int i=1;i<n;i++){
+		t=new node;
+		t->data=arr[i];
+		p->next=t;
+		t->next=head;
+		p=t;
+	}
+}
+//display elements of circular linked list
+void circularlinkedlist::display(){
+	node *p=head;
+	do{
+		cout<<p->data<<" ";
+		p=p->next;
+	}while(p!=head);
+}
+//main function
+int main(){
+	int arr[5]={2,7,8,10,13};
+	circularlinkedlist cll(arr,5);
+	cll.display();
+	return 0;
+	}
