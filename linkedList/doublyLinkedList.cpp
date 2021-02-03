fc1fb28e62e11f12835a3c42fc0ca77fe846242d
Ashi Sharma <57591881+ashi-sharma@users.noreply.github.com>
2021-02-03 23:22:27 +0530

    Update doublyLinkedList.cpp

--- linkedList/doublyLinkedList.cpp
+++ linkedList/doublyLinkedList.cpp
@@ -16,16 +16,50 @@ class dlinkedlist{
 		}
 		~dlinkedlist();
 		dlinkedlist(int arr[],int n);
+		void display();
+		void insert(int num,int index);
 };
+//insert an element at an index
+
+//display elements of doubly linked list
+void dlinkedlist::display(){
+	node *p=first;
+	while(p){
+		cout<<p->data<<" ";
+		p=p->next;
+	}
+}
 //destructor
 dlinkedlist::~dlinkedlist(){
-	
+	node *p=first;
+	while(first){
+		first=p->next;
+		delete p;
+		p=first;
+	}
 }
 //parameterised constructor
 dlinkedlist::dlinkedlist(int arr[],int n){
-	
+	first=new node;
+	node *t,*p;
+	first->prev=NULL;
+	first->data=arr[0];
+	p=first;
+	for(int i=1;i<n;i++){
+		p->next=new node;
+		t=p;
+		p=p->next;
+		p->prev=t;
+		p->data=arr[i];
+	}
+	p->next=NULL;
 }
 //main function
 int main(){
+	
+	int arr[5]={1,2,3,4,5};
+	dlinkedlist dll(arr,5);
+	dll.display();
 	return 0;
+	
 }
