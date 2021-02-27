b1b72513bc5319d3366d31a756c37c5072c9455f
Ashi Sharma <57591881+ashi-sharma@users.noreply.github.com>
2021-02-27 17:32:53 +0530

    Update trees.cpp

--- trees.cpp
+++ trees.cpp
@@ -1,1 +1,26 @@
-
+#include<iostream>
+using namespace std;
+//class node
+class node{
+	public:
+	node *lchild;
+	int data;
+	node *rchild;
+	node(){
+		lchild = NULL;
+		rchild = NULL;
+	}
+};
+//class tree
+class tree{
+	public:
+	node *root;
+	tree(){
+		root=NULL;
+	}
+	
+};
+int main(){
+	
+	return 0;
+}
