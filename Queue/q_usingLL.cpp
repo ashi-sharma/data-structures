00244c3b639670fa7f150c068b94d12ce70ad96c
Ashi Sharma <57591881+ashi-sharma@users.noreply.github.com>
2021-03-03 23:10:03 +0530

    Update q_usingLL.cpp

--- Queue/q_usingLL.cpp
+++ Queue/q_usingLL.cpp
@@ -10,9 +10,61 @@ class queue{
 		node *front;
 		node *rear;
 	public:
-		
+		queue(){
+			front=rear=NULL;
+		}
+		void enqueue(int x);
+		int dequeue();
+		void display();
 };
+void queue::enqueue(int x){
+	node *t = new node;
+	if(t==NULL){
+		cout<<"queue overflow"<<endl;
+	}
+	else{
+		t->data=x;
+		t->next=NULL;
+		if(front==NULL){
+			front=t;
+			rear=t;
+		}
+		else{
+			rear->next=t;
+			rear=t;
+		}
+	}
+}
+int queue::dequeue(){
+	int x=-1;
+	node *p;
+	if(front==NULL){
+		cout<<"queue is empty"<<endl;
+	}
+	else{
+		p=front;
+		front=p->next;
+		x=p->data;
+		delete p;
+	}
+	return x;
+}
+void queue::display(){
+	node *p=front;
+	while(p){
+		cout<<p->data<<" ";
+		p=p->next;
+	}
+	cout<<endl;
+}
 int main(){
-	
+	queue q;
+	q.enqueue(12);
+	q.enqueue(11);
+	q.display();
+	cout<<q.dequeue()<<"deleted"<<endl;
+	q.enqueue(16);
+	q.enqueue(17);
+	q.display();
 	return 0;
 }
